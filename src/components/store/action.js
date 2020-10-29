f5ac9d3d560ce4baf7a17f5774100770838bc107
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-10-29 12:58:39 +0500

    Update action.js

--- src/components/store/action.js
+++ src/components/store/action.js
@@ -1,2 +1,10 @@
 export const increment = 'INCREMENT';
 export const decrement = 'DECREMENT';
+
+export const increValue = () => ({
+    type: increment
+})
+
+export const decreValue = () => ({
+    type: decrement
+})
