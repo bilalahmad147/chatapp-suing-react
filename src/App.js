0f7a22e7be66ca936f3aa8ada0d82bfb27dd9b7b
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-10-29 12:58:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './App.css';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { increValue, decreValue } from './components/store/action';
 
 function App() {
 
   const state = useSelector(state => state)
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
       <h1>hello world</h1>
-      <h1>Counter: {state.name}</h1>
+      <h1>Counter: {state.counter}</h1>
+      <button onClick={() => dispatch(increValue())}>Plus</button>
+      <button onClick={() => dispatch(decreValue())}>Minus</button>
+
     </div>
   );
 }
