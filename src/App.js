34256948e5d4210f54d21248b80538c6ace13892
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-12 20:11:40 +0500

    create react app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
-import { increValue, decreValue } from './components/store/action';
 
 function App() {
-
-  const state = useSelector(state => state)
-  const dispatch = useDispatch()
 
   return (
     <div className="App">
       <h1>hello world</h1>
-      <h1>Counter: {state.counter}</h1>
-      <button onClick={() => dispatch(increValue())}>Plus</button>
-      <button onClick={() => dispatch(decreValue())}>Minus</button>
-
     </div>
   );
 }
--- src/components/store/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import CounterReducer from './reducer'
-
-const store = createStore(CounterReducer);
-
-export default store;
